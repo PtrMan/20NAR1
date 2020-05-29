efd3ac6203858d9214de18364da36f8e7b5d9950
PtrMan <1067920+PtrMan@users.noreply.github.com>
2020-05-29 10:52:06 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+#![allow(non_snake_case)]
+
+extern crate rand;
+
+//use std::default::Default;
+
+use rand::Rng;
+//use rand::distributions::{Normal, Distribution};
+
+// run example of GA
+pub fn main() {
+    let mut rng = rand::thread_rng();
+    
+    for iTask in 0..20 { // iterate over tasks
+            
+    for iRun in 0..50000 {
+        //(rng.gen::<f64>() * 2.0 - 1.0) * 0.5;
+        
+    
+        let mut v = vec![0; 8];
+        // init with random
+        for idx in 0..v.len() {
+            v[idx] = (rng.gen::<f64>() * 20.0) as i32;//((6059512.42149 * ((((idx + 5994) as f64) + 63.563)).powf(2.0)) % 50.0) as i32;
+        }
+        
+        let mut ctx = Ctx{ip:0,accu:0,accu2:0};
+        for iStep in 0..8 {
+            let ip = ctx.ip;
+            let instr = v[ip as usize];
+            //println!("ip {}  instr {}", ip, instr);
+            interp(&mut ctx, instr);
+        }
+        
+        //println!("res {} {}", ctx.accu, ctx.accu2);
+        
+        let targetValue = iTask; // the target value must be the task id - easy to find entropy this way
+        if ctx.accu == targetValue {
+            println!("found solution! {}", targetValue);
+            break;
+        }
+    }
+    
+    }
+
+}
+
+pub struct Ctx {
+    ip:i32,
+    accu:i32, // accumulator
+    accu2:i32,
+}
+
+// interpret instruction
+pub fn interp(ctx:&mut Ctx, instr:i32) {
+    let mut incIp=true;
+    
+    match instr {
+        // Match a single value
+        0 => {}, // nop
+        1 => ctx.accu = 0, // reset accu
+        2 => ctx.accu = 1, // set accu to one
+        3 => ctx.accu+=1,
+        4 => ctx.accu-=1,
+        5 => { // xchg
+            let t = ctx.accu;
+            ctx.accu=ctx.accu2;
+            ctx.accu2=t;
+        },
+        6 => ctx.accu=ctx.accu+ctx.accu2,
+        7 => ctx.accu=ctx.accu-ctx.accu2,
+        8 => ctx.accu=ctx.accu*ctx.accu2,
+        9 => ctx.accu=5,
+        
+        20..=40 => {incIp=false; ctx.ip = instr-20}, // jump absolute
+        // Handle the rest of cases
+        _ => {},
+    }
+
+    if incIp {
+        ctx.ip+=1;
+    }
+}
