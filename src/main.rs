6c537bc974e42ac072ff794e12ea2fd2c428ea9a
PtrMan <1067920+PtrMan@users.noreply.github.com>
2020-05-30 12:00:21 +0200

    update: solving old problems when all new ones got solved to 'validate' the solver

--- src/main.rs
+++ src/main.rs
@@ -26,11 +26,11 @@ pub fn main() {
 
 // TODO< implement peripherial vision too >
 pub fn expInvent0() {
-    
+    let mut solvedProblems:Vec<Box<dyn ProblemInstance>> = vec![];
 
     // we generate problems on the fly, so we need to iterate over them!
     for iProblem in 0..2 { // iterate over more and more difficult problems
-        solveProblem(iProblem);
+        solveProblem(iProblem, &mut solvedProblems);
     }
 
 
@@ -42,7 +42,7 @@ pub fn expInvent0() {
 // inner loop which does the problem solving.
 // the source of the problem is a problem generator, which generates more and more difficult problems
 // /param problemNr number of the problem, starting from zero
-pub fn solveProblem(problemNr:i32) {
+pub fn solveProblem(problemNr:i32, solvedProblems:&mut Vec<Box<dyn ProblemInstance>>) {
     let mut rng = rand::thread_rng();
 
     println!("- search for NN which solves the task in this environment");
@@ -52,17 +52,17 @@ pub fn solveProblem(problemNr:i32) {
 
 
 
-    let mut currentProblems:Vec<CursorProblem> = Vec::<CursorProblem>::new(); // problem instances which it needs to all solve with one single modified agent
+    let mut currentProblems = Vec::<Box<dyn ProblemInstance>>::new(); // problem instances which it needs to all solve with one single modified agent
     { // create problems
         for _iVersion in 0..12 { // iterate over version of the same problem
             let mut boxX0 = 0; // position of the drawn box
             let problemDifficulty:i32 = problemNr; // the problem difficulty
             let problemMap:map2d::Map2d::<f64> = invent0(problemDifficulty, &mut boxX0); // invent a map of the problem
 
-            currentProblems.push(CursorProblem {
+            currentProblems.push(Box::new(CursorProblem {
                 boxX0:boxX0,
                 problemMap:problemMap,
-            });
+            }));
         }
     }
 
@@ -127,9 +127,11 @@ pub fn solveProblem(problemNr:i32) {
             });
         }
 
-        for iEnvStimulusVersion in 0..currentProblems.len() {
-            let iProblem = &currentProblems[iEnvStimulusVersion];
-
+        let nProblems = currentProblems.len()+solvedProblems.len();
+        for iEnvStimulusVersion in 0..nProblems {
+            let iProblem =
+                if iEnvStimulusVersion < currentProblems.len() {&currentProblems[iEnvStimulusVersion]} // select from current problem
+                else {&solvedProblems[iEnvStimulusVersion-currentProblems.len()]}; // select from solved problem because we need to solve them too!
 
             let mut solverState:SolverState = SolverState {
                 cursorX : 3,
@@ -140,8 +142,16 @@ pub fn solveProblem(problemNr:i32) {
             if !isSolved {
                 break; // we don't need to continue if unsolved
             }
-            if isSolved && iEnvStimulusVersion == currentProblems.len() - 1 { // were all problems solved?
+            if isSolved && iEnvStimulusVersion == nProblems - 1 { // were all problems solved?
+                { // store all problems because we solved them
+                    for icurrentProblem in currentProblems {
+                        solvedProblems.push(icurrentProblem);
+                    }
+                }
+                
                 println!("problem#{} steps={} v#{}  archived  F I N A L  goal!", problemNr, iNnSearchStep, iEnvStimulusVersion);
+                
+                
                 return;
             }
             println!("problem#{} steps={} v#{}  archived goal!", problemNr, iNnSearchStep, iEnvStimulusVersion);
@@ -265,11 +275,11 @@ pub fn invent0(problemDifficulty:i32, boxX0:&mut i32) -> map2d::Map2d::<f64> {
 
         let mut boxY0 = 0;
         if problemDifficulty > 0 {
-            boxY0 = v[1] % (map.h-1); // let y position depend on random var in this more complicated case!
+            boxY0 = v[1] % (map.h-1-4); // let y position depend on random var in this more complicated case!
         }
 
         // interpret genes to draw
-        *boxX0 = v[0] % (map.w-1); // we need to write the value outside
+        *boxX0 = v[0] % (map.w-1-4); // we need to write the value outside
         //map2d::map2dDrawBox(&mut map, *boxX0 ,v[1],v[2],v[3],1.0); // commented becaus it was to random for simple difficulty
         
         if problemDifficulty >= 2 {
@@ -284,7 +294,7 @@ pub fn invent0(problemDifficulty:i32, boxX0:&mut i32) -> map2d::Map2d::<f64> {
 
 
         // DEBUG print to console
-        if false {
+        if problemDifficulty > 0 {
             for iy in 0..map.h {
                 for ix in 0..map.w {
                     if map2d::readAt(&map, iy,ix) > 0.5 {print!("x");}
@@ -293,6 +303,7 @@ pub fn invent0(problemDifficulty:i32, boxX0:&mut i32) -> map2d::Map2d::<f64> {
                 }
                 println!();
             }
+            println!("---");
         }
 
 
@@ -343,118 +354,3 @@ pub trait ProblemInstance {
 
 
 
-
-// run example of GA
-pub fn expGa0() {
-    let mut rng = rand::thread_rng();
-
-    // found solution programs
-    let mut solutions:Vec<Solution0> = vec![];
-    
-    for iTask in 0..50 { // iterate over tasks
-            
-        for _iRun in 0..50000 {
-            //(rng.gen::<f64>() * 2.0 - 1.0) * 0.5;
-            
-        
-            let mut v = vec![0; 9];
-            // init with random
-            for idx in 0..v.len() {
-                v[idx] = (rng.gen::<f64>() * 11.0) as i32;//((6059512.42149 * ((((idx + 5994) as f64) + 63.563)).powf(2.0)) % 50.0) as i32;
-            }
-            
-            let mut ctx = Ctx{ip:0,accu:0,accu2:0,accuf:0.0,accu2f:0.0};
-            for _iStep in 0..9 {
-                let ip = ctx.ip;
-                let instr = v[ip as usize];
-                //println!("ip {}  instr {}", ip, instr);
-                interp(&mut ctx, instr);
-            }
-            
-            //println!("res {} {}", ctx.accu, ctx.accu2);
-            
-            let targetValue = iTask; // the target value must be the task id - easy to find entropy this way
-            if ctx.accu == targetValue {
-                println!("found solution! {}", targetValue);
-
-                // store solution
-                solutions.push(Solution0{prgm:v.clone()});
-
-                break;
-            }
-        }
-    
-    }
-
-    // print solutions
-    {
-        for iSolution in solutions {
-            println!("{:?}", iSolution.prgm);
-        }
-    }
-
-}
-
-// context to store interpretation state
-pub struct Ctx {
-    ip:i32,
-    accu:i32, // accumulator
-    accu2:i32,
-    accuf:f64, // float accumulator
-    accu2f:f64,
-}
-
-// interpret instruction
-pub fn interp(ctx:&mut Ctx, instr:i32) {
-    let mut incIp=true;
-    
-    match instr {
-        // page 0 : basic integer operations
-        0 => {}, // nop
-        1 => ctx.accu = 0, // reset accu
-        2 => ctx.accu = 1, // set accu to one
-        3 => ctx.accu+=1,
-        4 => ctx.accu-=1,
-        5 => { // xchg
-            let t = ctx.accu;
-            ctx.accu=ctx.accu2;
-            ctx.accu2=t;
-        },
-        6 => ctx.accu=ctx.accu+ctx.accu2,
-        7 => ctx.accu=ctx.accu-ctx.accu2,
-        8 => ctx.accu=ctx.accu*ctx.accu2,
-        9 => ctx.accu=5,
-
-        // page 1 : basic float operations
-
-        10 => {}, // TODO< use >
-
-        11 => ctx.accuf = 0.0, // reset accu
-        12 => ctx.accuf = 1.0, // set accu to one
-        13 => ctx.accuf+=1.0,
-        14 => ctx.accuf-=1.0,
-        15 => { // xchg
-            let t = ctx.accuf;
-            ctx.accuf=ctx.accu2f;
-            ctx.accu2f=t;
-        },
-        16 => ctx.accuf=ctx.accuf+ctx.accu2f,
-        17 => ctx.accuf=ctx.accuf-ctx.accu2f,
-        18 => ctx.accuf=ctx.accuf*ctx.accu2f,
-        19 => ctx.accuf=5.0,
-        
-        40..=60 => {incIp=false; ctx.ip = instr-20}, // jump absolute
-        // Handle the rest of cases
-        _ => {},
-    }
-
-    if incIp {
-        ctx.ip+=1;
-    }
-}
-
-
-// structure to store solution
-struct Solution0 {
-    prgm: Vec<i32>, // program
-}
