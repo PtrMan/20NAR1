f714923c23299eca26dbd172b2307e1b11d47a6a
PtrMan <1067920+PtrMan@users.noreply.github.com>
2020-10-18 00:14:18 +0200

    update: Binding: reading from NAR works now too

--- Binding.py
+++ Binding.py
@@ -1,13 +1,29 @@
 # Binding.py
 # python binding to use reasoner from python
+# requires python 3.x
+
+from queue import Queue, Empty
 
 class Binding(object):
     # /param pathToNar path to the NAR, ex: "/home/r0b3/dev/rust/20mlish6"
     def __init__(self, pathToNar):
         import subprocess
+        from threading import Thread
 
         # universal_newlines because we want text output
         self.proc = subprocess.Popen(["cargo", "run", "--release", "it"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True, cwd=pathToNar)
+
+        # we need queue for reading
+        # see https://stackoverflow.com/a/4896288/388614
+        def enqueueOutput(out, queue):
+            for line in iter(out.readline, b''):
+                queue.put(line)
+            out.close()
+        
+        self.queue = Queue()
+        t = Thread(target=enqueueOutput, args=(self.proc.stdout, self.queue))
+        t.daemon = True # thread dies with the program
+        t.start()
 
     # input
     def i(self, text):
@@ -23,5 +39,13 @@ class Binding(object):
     
     def s(self):
         self.i("!s")
-
-# TODO< add receiving from subprocess >
+    
+    # try to read, returns None if nothing was returned
+    def tryRead(self):
+        # read line without blocking
+        try:
+            line = self.queue.get_nowait()
+        except Empty:
+            return None
+        else:
+            return line
