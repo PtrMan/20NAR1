c1b61060dfc52f813a31a7eb90b8fd81923f9177
PtrMan <1067920+PtrMan@users.noreply.github.com>
2020-12-20 23:17:56 +0100

    update: ^nlpRel0 adds knowledge to memory

--- src/OpLib.rs
+++ src/OpLib.rs
@@ -2,13 +2,17 @@
 
 use std::rc::Rc;
 use std::sync::Arc;
+use std::sync::atomic::{Ordering};
 use parking_lot::RwLock;
 
 use crate::NarProc;
 use crate::Term::{Term, convTermToStr, checkEqTerm, Copula};
 use crate::TermUtils::decodeOp;
 use crate::NarMem;
-use crate::NarWorkingCycle::Mem2;
+use crate::NarWorkingCycle;
+use crate::NarSentence::*;
+use crate::NarStamp;
+use crate::Tv;
 
 // ops for pong environment
 pub struct OpNop {
@@ -19,7 +23,7 @@ impl NarProc::Op for OpNop {
     fn retName(&self) -> String {
         self.name.clone()
     }
-    fn call(&self, _nar:&mut NarProc::ProcNar, _narMem:&Option<Arc<RwLock<Mem2>>>, _args:&Vec<Term>) {
+    fn call(&self, _nar:&mut NarProc::ProcNar, _narMem:&Option<Arc<RwLock<NarWorkingCycle::Mem2>>>, _args:&Vec<Term>) {
     }
     fn isBabbleable(&self) -> bool {true}
 }
@@ -35,7 +39,7 @@ impl NarProc::Op for Op_nal9__exec_and_inject {
     fn retName(&self) -> String {
         "^nal9_exeAndInject".to_string()
     }
-    fn call(&self, nar:&mut NarProc::ProcNar, narMem:&Option<Arc<RwLock<Mem2>>>, args:&Vec<Term>) {
+    fn call(&self, nar:&mut NarProc::ProcNar, narMem:&Option<Arc<RwLock<NarWorkingCycle::Mem2>>>, args:&Vec<Term>) {
         println!("ENTER");
 
         if args.len() != 3 {
@@ -85,7 +89,7 @@ impl NarProc::Op for Op__nlp_rel_0 {
     fn retName(&self) -> String {
         "^nlpRel0".to_string()
     }
-    fn call(&self, nar:&mut NarProc::ProcNar, narMem:&Option<Arc<RwLock<Mem2>>>, args:&Vec<Term>) {
+    fn call(&self, nar:&mut NarProc::ProcNar, declMem:&Option<Arc<RwLock<NarWorkingCycle::Mem2>>>, args:&Vec<Term>) {
         println!("ENTER NLP rel 0");
 
         if args.len() != 2 {
@@ -124,8 +128,27 @@ impl NarProc::Op for Op__nlp_rel_0 {
 
         println!("H rel {}", &convTermToStr(&rel)); // print relation
         println!("H rewritten rel {}", &convTermToStr(&rewrittenRel)); // print relation
-        
-        //TODO< add rewrittenRel relation to declarative memory! >
+
+
+        // add rewrittenRel to memory
+        match declMem {
+            Some(mem) => {
+                let sentence: SentenceDummy = {
+                    let stampId:i64 =mem.read().shared.read().stampIdCounter.fetch_add(1, Ordering::SeqCst); // TODO< is this ordering ok? >
+                    let stamp = NarStamp::newStamp(&vec![stampId]);
+                    newEternalSentenceByTv(&rewrittenRel,EnumPunctation::JUGEMENT,&Tv::Tv{f:1.0,c:0.9},stamp)
+                };
+
+                let memWrite = mem.write();
+                
+                // HACK< belongs into decl memory >
+                let cfg__maxComplexity = 64;
+                let cfg__nConceptBeliefs = 60;
+
+                NarWorkingCycle::memAddTask(Arc::clone(&memWrite.shared), &sentence, true, cfg__maxComplexity, cfg__nConceptBeliefs, 1.0);
+            },
+            None => {} // can't add to decl memory, ignore
+        }
     }
     fn isBabbleable(&self) -> bool {false} // can't be used for babbling because it is not useful to babble it
 }
